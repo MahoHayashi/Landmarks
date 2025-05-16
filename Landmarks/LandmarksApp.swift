725f61b98c75d38747097257de133811c3c0881c
hayashi maho <is0792vf@ed.ritsmei.ac.jp>
2025-05-16 23:59:21 +0900

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by maho hayashi on 2025/05/16.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
