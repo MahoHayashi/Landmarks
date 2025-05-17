84af6da94ea8684114e5c53f9394e2584f124ec4
hayashi maho <is0792vf@ed.ritsmei.ac.jp>
2025-05-17 17:22:17 +0900

    finish chapter2

---
+++ Landmarks/Helper/CircleImage.swift
@@ -0,0 +1,25 @@
+//
+//  CircleImage.swift
+//  Landmarks
+//
+//  Created by maho hayashi on 2025/05/17.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var image: Image
+    
+    var body: some View {
+        image
+            .clipShape(Circle())
+            .overlay {
+                Circle().stroke(.white, lineWidth: 4)
+            }
+            .shadow(radius: 7)
+    }
+}
+
+#Preview {
+    CircleImage(image: Image("turtlerock"))
+}
