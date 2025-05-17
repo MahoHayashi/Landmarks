9ee9f2a7cecb0ca01a31b4c1dbf2f7e1b3af1a27
hayashi maho <is0792vf@ed.ritsmei.ac.jp>
2025-05-17 20:05:07 +0900

    finish chapter3

--- Landmarks/Model/ModelData.swift
+++ Landmarks/Model/ModelData.swift
@@ -11,9 +11,22 @@ import Foundation
 class ModelData{
     var landmarks: [Landmark] = load("landmarkData.json")
     var hikes: [Hike] = load("hikeData.json")
+    var profile = Profile.default
+    
+    var features: [Landmark] {
+           landmarks.filter { $0.isFeatured }
+       }
+    
+    var categories: [String: [Landmark]] {
+        Dictionary(
+            grouping: landmarks,
+            by: { $0.category.rawValue }
+        )
+    }
+
 }
 
-var landmarks: [Landmark] = load("landmarkData.json")
+//var landmarks: [Landmark] = load("landmarkData.json")
 
 
 func load<T: Decodable>(_ filename: String) -> T {
